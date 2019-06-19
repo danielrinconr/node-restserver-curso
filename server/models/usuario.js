671d663a11ec58ce514b9bfdf08d4066c1193d98
Daniel <danielrinconr@usantotomas.edu.co>
2019-06-19 15:21:05 -0500

    Delete unnecesary lines

--- server/models/usuario.js
+++ server/models/usuario.js
@@ -1,16 +1,10 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
-
-
 let rolesValidos = {
     values: ['ADMIN_ROLE', 'USER_ROLE'],
     message: '{VALUE} no es un rol válido'
 };
-
-
 let Schema = mongoose.Schema;
-
-
 let usuarioSchema = new Schema({
     nombre: {
         type: String,
@@ -43,19 +37,11 @@ let usuarioSchema = new Schema({
         default: false
     }
 });
-
-
 usuarioSchema.methods.toJSON = function() {
-
     let user = this;
     let userObject = user.toObject();
     delete userObject.password;
-
     return userObject;
 }
-
-
 usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe de ser único' });
-
-
 module.exports = mongoose.model('Usuario', usuarioSchema);
